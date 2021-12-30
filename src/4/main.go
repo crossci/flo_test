155d6c1cbbb7d4198114af9d80982ddb75d5d097
朱昆昆 <zhukunkun@gillar.com>
2021-12-30 14:41:33 +0800

    使用enum定义四个方向

--- src/4/main.go
+++ src/4/main.go
@@ -1,5 +1,12 @@
 package main
 
+const (
+	LEFT  int8 = 0 //
+	UP    int8 = 1 //
+	RIGHT int8 = 2 //
+	DOWN  int8 = 3 //
+)
+
 //边
 type side struct {
 	t int8
@@ -14,42 +21,42 @@ func check_end(blocks []*block, w, h int32) bool {
 	for i, b := range blocks {
 		nbs := get_near_blocks(blocks, int32(i), w, h)
 		//左
-		if nbs[0] != nil {
-			if nbs[0].sides[2].v+b.sides[0].v != 0 {
+		if nbs[LEFT] != nil {
+			if nbs[LEFT].sides[RIGHT].v+b.sides[LEFT].v != 0 {
 				return false
 			}
 		} else {
-			if b.sides[0].v != 0 {
+			if b.sides[LEFT].v != 0 {
 				return false
 			}
 		}
 		//上
-		if nbs[1] != nil {
-			if nbs[0].sides[3].v+b.sides[1].v != 0 {
+		if nbs[UP] != nil {
+			if nbs[UP].sides[DOWN].v+b.sides[UP].v != 0 {
 				return false
 			}
 		} else {
-			if b.sides[1].v != 0 {
+			if b.sides[UP].v != 0 {
 				return false
 			}
 		}
 		//右
-		if nbs[2] != nil {
-			if nbs[2].sides[0].v+b.sides[2].v != 0 {
+		if nbs[RIGHT] != nil {
+			if nbs[RIGHT].sides[LEFT].v+b.sides[RIGHT].v != 0 {
 				return false
 			}
 		} else {
-			if b.sides[2].v != 0 {
+			if b.sides[RIGHT].v != 0 {
 				return false
 			}
 		}
 		//下
-		if nbs[3] != nil {
-			if nbs[3].sides[1].v+b.sides[3].v != 0 {
+		if nbs[DOWN] != nil {
+			if nbs[DOWN].sides[UP].v+b.sides[DOWN].v != 0 {
 				return false
 			}
 		} else {
-			if b.sides[3].v != 0 {
+			if b.sides[DOWN].v != 0 {
 				return false
 			}
 		}
@@ -62,21 +69,21 @@ func get_near_blocks(blocks []*block, i, w, h int32) []*block {
 	y := get_x(i, w, h)
 	x1 := x - 1
 	if x1 >= 0 {
-		ret[0] = blocks[get_index(x1, y, w)]
+		ret[LEFT] = blocks[get_index(x1, y, w)]
 	}
 	y1 := y - 1
 	if y1 >= 0 {
-		ret[1] = blocks[get_index(x, y1, w)]
+		ret[UP] = blocks[get_index(x, y1, w)]
 	}
 
 	x1 = x + 1
 	if x1 < w {
-		ret[2] = blocks[get_index(x1, y, w)]
+		ret[RIGHT] = blocks[get_index(x1, y, w)]
 	}
 
 	y1 = y + 1
 	if y1 < h {
-		ret[3] = blocks[get_index(x, y1, w)]
+		ret[DOWN] = blocks[get_index(x, y1, w)]
 	}
 	return ret
 }
